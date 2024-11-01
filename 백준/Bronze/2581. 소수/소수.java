474102ba1ee112b3ffe825f1c943f958e20bad31
시온 <99112680+Sionparadox@users.noreply.github.com>
2024-11-01 14:13:10 +0900

    [Bronze II] Title: 소수, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -26,7 +26,7 @@ public class Main {
 			return false;
 		if (n == 2)
 			return true;
-		for (int i = 2; i < n; i++) {
+		for (int i = 2; i < Math.sqrt(n) + 1; i++) {
 			if (n % i == 0) {
 				return false;
 			}
