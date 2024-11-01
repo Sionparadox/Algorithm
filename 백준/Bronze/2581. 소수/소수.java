ba4eacd948191b83898e8aa384bfc51389d93d59
시온 <99112680+Sionparadox@users.noreply.github.com>
2024-11-01 14:12:01 +0900

    [Bronze II] Title: 소수, Time: 196 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		int minPrime = 0;
+		int sumPrime = 0;
+		for (int i = N; i >= M; i--) {
+			if (isPrime(i)) {
+				sumPrime += i;
+				minPrime = i;
+			}
+		}
+		if (minPrime == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(sumPrime);
+			System.out.println(minPrime);
+		}
+	}
+
+	private static boolean isPrime(int n) {
+		if (n == 1)
+			return false;
+		if (n == 2)
+			return true;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
