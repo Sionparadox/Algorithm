25ccdb7177d3e920fab916ce7e340c13df6515d4
시온 <99112680+Sionparadox@users.noreply.github.com>
2024-11-05 10:36:50 +0900

    [Silver III] Title: 주유소, Time: 864 ms, Memory: 176932 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] distances = new int[N - 1];
+		int[] oilPrices = new int[N];
+		for (int i = 0; i < N - 1; i++) {
+			distances[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			oilPrices[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N - 1; i++) {
+			if (oilPrices[i] < oilPrices[i + 1]) {
+				oilPrices[i + 1] = oilPrices[i];
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < N - 1; i++) {
+			ans += distances[i] * oilPrices[i];
+		}
+		System.out.println(ans);
+	}
+
+}
