40425c4d7546bc88443819092d7b2e38b412f38a
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-02-27 16:51:55 +0900

    [Silver I] Title: 볼 모으기, Time: 444 ms, Memory: 35168 KB, Score: 51 point -BaekjoonHub

---
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String balls = sc.next();
+		LinkedList<Integer> ballLen = new LinkedList<>();
+		ballLen.add(0);
+		char prev = balls.charAt(0);
+		for (char b : balls.toCharArray()) {
+			if (b == prev) {
+				ballLen.set(ballLen.size() - 1, ballLen.getLast() + 1);
+			} else {
+				prev = b;
+				ballLen.add(1);
+			}
+		}
+		if (ballLen.size() <= 2) {
+			System.out.println(0);
+			return;
+		}
+		int oddSum = 0;
+		int evenSum = 0;
+		for (int i = 1; i < ballLen.size() - 1; i++) {
+			if (i % 2 == 0) {
+				oddSum += ballLen.get(i);
+			} else {
+				evenSum += ballLen.get(i);
+			}
+		}
+		oddSum += ballLen.size() % 2 == 0 ? 0 : Math.min(ballLen.getFirst(), ballLen.getLast());
+		System.out.println(Math.min(evenSum, oddSum));
+	}
+}
