b051efe050822dbe883e13a19c8365ab8233960a
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-03-10 08:29:27 +0900

    [Silver II] Title: 편의점 2, Time: 400 ms, Memory: 50904 KB -BaekjoonHub

---
+++ 백준/Silver/14400. 편의점 2/편의점 2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = parseInt(input[0]);
+const pos = [];
+
+// 입력 형식에 맞게 조정
+for (let i = 1; i <= N; i++) {
+  pos.push(input[i].split(' ').map(Number));
+}
+
+const x = pos.map(p => p[0]);
+const y = pos.map(p => p[1]);
+
+x.sort((a, b) => a - b);
+y.sort((a, b) => a - b);
+
+const mid = Math.floor(N / 2);
+
+let ans = 0;
+for (let i = 0; i < N; i++) {
+  ans += Math.abs(x[i] - x[mid]);
+  ans += Math.abs(y[i] - y[mid]);
+}
+
+console.log(ans);
