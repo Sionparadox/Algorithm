10b71fda64ef406d81fe39457bef092e3cdb8da5
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-03-30 17:26:44 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 184 ms, Memory: 12076 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+// const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+const [[N, M], ...arr] = input.map((i) => i.split(' ').map(Number));
+
+const distance = Array.from({ length: N + 1 }, () =>
+  Array(N + 1).fill(Infinity)
+);
+for (let i = 0; i <= N; i++) {
+  distance[i][i] = 0;
+}
+
+for (const [u, v] of arr) {
+  distance[u][v] = 1;
+  distance[v][u] = 1;
+}
+
+for (let k = 1; k <= N; k++) {
+  for (let i = 1; i < N; i++) {
+    for (let j = i + 1; j <= N; j++) {
+      distance[i][j] = Math.min(
+        distance[i][j],
+        distance[i][k] + distance[k][j]
+      );
+      distance[j][i] = Math.min(
+        distance[j][i],
+        distance[j][k] + distance[k][i]
+      );
+    }
+  }
+}
+let min = Infinity;
+let idx = -1;
+for (let i = 1; i <= N; i++) {
+  const sum = distance[i].slice(1).reduce((acc, v) => acc + v, 0);
+  if (sum < min) {
+    min = sum;
+    idx = i;
+  }
+}
+console.log(idx);
