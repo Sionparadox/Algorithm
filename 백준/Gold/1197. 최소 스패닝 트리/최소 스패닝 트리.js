cf8f933efd32a245a582c5a9bfce1434594d27b7
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-04-30 12:11:12 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 392 ms, Memory: 61504 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.js
@@ -1,28 +1,27 @@
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 const [V,E] = input[0].split(' ').map(Number);
-const lines = input.slice(1).map(line => line.split(' ').map(Number));
-lines.sort((a,b) => a[2] - b[2]);
-const parent = Array(V+1).fill().map((_,i)=>i);
+const arr = input.slice(1).map(item => item.split(' ').map(Number));
+arr.sort((a,b) => a[2] - b[2]);
 
-let mstWeight = 0;
-for (const [u,v,w] of lines){
+const parent = Array.from({length:V+1}, (_,i) => i);
+
+let ans = 0;
+for (const [u,v,w] of arr){
     if(find(u) != find(v)){
-        union(u,v);
-        mstWeight += w;
+        union(u, v);
+        ans += w;
     }
 }
-console.log(mstWeight);
+console.log(ans);
 
 function find(node){
     if(parent[node] == node) return node;
     return parent[node] = find(parent[node]);
 }
 
-function union(u,v){
+function union(u, v){
     const rootU = find(u);
     const rootV = find(v);
-    if(rootU != rootV){
-        parent[rootU] = rootV;
-    }
+    if(rootU != rootV) parent[rootU] = rootV;
 }
