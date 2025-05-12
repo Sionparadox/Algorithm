d5b5de12b8288dfebf1ed67a01766fe4b484503f
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-05-12 11:44:35 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9588 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 const L = +input[0];
 
+const M = 1234567891
 let ans = 0;
+let R = 1;
 for (let i=0; i<L; i++){
-    ans += (input[1].charCodeAt(i)-96)*Math.pow(31, i) % 1234567891;
-    ans %= 1234567891
+    ans = (ans + (input[1].charCodeAt(i)-96) * R) % M;
+    R = (R * 31) % M;
 }
 console.log(ans);
