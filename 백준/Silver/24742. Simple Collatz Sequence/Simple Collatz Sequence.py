0bc18fdf0480cf90afd50d7b9a4271877c212a54
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-05-29 17:09:10 +0900

    [Silver I] Title: Simple Collatz Sequence, Time: 44 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Silver/24742. Simple Collatz Sequence/Simple Collatz Sequence.py
+++ 백준/Silver/24742. Simple Collatz Sequence/Simple Collatz Sequence.py
@@ -3,15 +3,24 @@ input = sys.stdin.readline
 MOD = 1000007
 
 M = int(input())
-dp = [[0,0] for _ in range(M+1)]
-dp[0][0] = 0
-dp[0][1] = 1
 
-for i in range(M):
-    dp[i+1][0] = (dp[i][0] + dp[i][1])%MOD
-    dp[i+1][1] = dp[i][0]%MOD
+dp = [0]*(M+1)
+dp[0] = dp[1] = 1
+for i in range(2, M+1):
+    dp[i] = (dp[i-1] + dp[i-2])%MOD
+print(dp[M-1])
 
-print(sum(dp[M-1])%MOD)
+
+
+# dp = [[0,0] for _ in range(M+1)]
+# dp[0][0] = 0
+# dp[0][1] = 1
+
+# for i in range(M):
+    # dp[i+1][0] = (dp[i][0] + dp[i][1])%MOD
+    # dp[i+1][1] = dp[i][0]%MOD
+
+# print(sum(dp[M-1])%MOD)
 '''
 dp[i][0] : i번 연산으로 1이 되는 짝수의 개수
 dp[i][1] : i번 연산으로 1이 되는 홀수의 개수
