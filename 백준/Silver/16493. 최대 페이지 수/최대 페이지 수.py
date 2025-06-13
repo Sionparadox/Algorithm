926cd7873b28898c59ed448b467c8dcd11f8a181
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-06-13 18:24:03 +0900

    [Silver II] Title: 최대 페이지 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
@@ -8,9 +8,8 @@ dp = [0]*(N+1)
 for date, page in books:
     for i in range(N, date-1, -1):
         dp[i] = max(dp[i], dp[i-date]+page)
-        
 print(dp[N])
 '''
-dp[i] : i일동안 읽을 수 있는 최대 페이지 수
 
+dp[i] : i일동안 읽을 수 있는 최대 페이지 수
 '''
