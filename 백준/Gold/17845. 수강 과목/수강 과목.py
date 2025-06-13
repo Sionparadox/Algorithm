97ac3115b072a8ee3a67334b18cb99d5f32c46a0
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-06-13 17:20:58 +0900

    [Gold V] Title: 수강 과목, Time: 1892 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+importance = [0]*(K+1)
+time = [0]*(K+1)
+for k in range(1,K+1):
+    i, t = map(int, input().split())
+    importance[k] = i
+    time[k] = t
+
+dp = [0]*(N+1)
+for k in range(1,K+1):
+    imp = importance[k]
+    tm = time[k]
+    for i in range(N, tm-1, -1):
+        dp[i] = max(dp[i], dp[i-tm]+imp)
+
+print(dp[N])
+'''
+dp[i] : i 공부시간으로 얻을 수 있는 최대 중요도
+'''
