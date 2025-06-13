5c6065ad7e8c8e275ded9f9756d59901128177de
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-06-13 15:33:49 +0900

    [Gold III] Title: 양팔저울, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+weights = list(map(int, input().split()))
+M = int(input())
+balls = list(map(int, input().split()))
+
+dp = [set() for _ in range(N+1)]
+dp[0].add(0)
+
+for i in range(N):
+    w = weights[i]
+    for val in dp[i]:
+        dp[i+1].add(val)
+        dp[i+1].add(val+w)
+        dp[i+1].add(abs(val-w))
+
+answer = []
+for ball in balls:
+    if ball in dp[N]:
+        answer.append('Y')
+    else:
+        answer.append('N')
+
+print(' '.join(answer))
+'''
+dp[i] : i무게의 구슬까지 탐색했을 때 가능한 무게들
+
+'''
