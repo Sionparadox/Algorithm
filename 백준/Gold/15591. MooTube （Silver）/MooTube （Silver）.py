8f4ac51c1c6fb0db31423ccbc1e44b0c0901d596
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-06-27 18:21:40 +0900

    [Gold V] Title: MooTube (Silver), Time: 60 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -1,4 +1,56 @@
 import sys
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+
+parent = [x for x in range(N+1)]
+size = [1]*(N+1)
+
+def find(node):
+    if node == parent[node]:
+        return node
+    parent[node] = find(parent[node])
+    return parent[node]
+
+def union(u, v):
+    rootU = find(u)
+    rootV = find(v)
+    if rootU == rootV:
+        return
+    if size[rootU] < size[rootV]:
+        rootU, rootV = rootV, rootU
+    parent[rootV] = rootU
+    size[rootU] += size[rootV]
+
+graph = []
+for _ in range(N-1):
+    u, v, w = map(int, input().split())
+    graph.append((w, u, v))
+
+graph.sort(reverse=True)
+    
+question = []
+for i in range(Q):
+    k, v = map(int, input().split())
+    question.append((k, v, i))
+
+question.sort(reverse=True)
+
+answer = [0]*Q
+idx = 0
+for k, v, i in question:
+    while idx<N-1 and graph[idx][0]>=k:
+        _, a, b = graph[idx]
+        union(a, b)
+        idx += 1
+    answer[i] += size[find(v)] -1
+    
+for ans in answer:
+    print(ans)   
+
+
+'''
+import sys
 from collections import defaultdict, deque
 input = sys.stdin.readline
 
@@ -29,54 +81,4 @@ for _ in range(Q):
     k, v = map(int, input().split())
     print(BFS(k, v))
 
-
-# import sys
-# input = sys.stdin.readline
-
-# N, Q = map(int, input().split())
-
-# parent = [x for x in range(N+1)]
-# size = [1]*(N+1)
-
-# def find(node):
-    # if node == parent[node]:
-        # return node
-    # parent[node] = find(parent[node])
-    # return parent[node]
-
-# def union(u, v):
-    # rootU = find(u)
-    # rootV = find(v)
-    # if rootU == rootV:
-        # return
-    # if size[rootU] < size[rootV]:
-        # rootU, rootV = rootV, rootU
-    # parent[rootV] = rootU
-    # size[rootU] += size[rootV]
-
-# graph = []
-# for _ in range(N-1):
-    # u, v, w = map(int, input().split())
-    # graph.append((w, u, v))
-
-# graph.sort(reverse=True)
-    
-# question = []
-# for i in range(Q):
-    # k, v = map(int, input().split())
-    # question.append((k, v, i))
-
-# question.sort(reverse=True)
-
-# answer = [0]*Q
-# idx = 0
-# for k, v, i in question:
-    # while idx<N-1 and graph[idx][0]>=k:
-        # _, a, b = graph[idx]
-        # union(a, b)
-        # idx += 1
-    # answer[i] += size[find(v)] -1
-    
-# for ans in answer:
-    # print(ans)   
-    
+'''  
