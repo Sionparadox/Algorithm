935895d5fac1006ef55f298542946a29900b7aed
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-07-03 16:10:19 +0900

    [Gold II] Title: 닭싸움 팀 정하기, Time: 284 ms, Memory: 34976 KB -BaekjoonHub

---
+++ 백준/Gold/1765. 닭싸움 팀 정하기/닭싸움 팀 정하기.py
@@ -0,0 +1,42 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+
+enemy = defaultdict(list)
+parent = [x for x in range(N+1)]
+
+def find(node):
+    if node == parent[node]:
+        return node
+    parent[node] = find(parent[node])
+    return parent[node]
+
+def union(u, v):
+    rootU, rootV = find(u), find(v)
+    if rootU != rootV:
+        parent[rootV] = rootU
+
+for _ in range(M):
+    type, u, v = input().split()
+    u, v = int(u), int(v)
+    if type == 'F':
+        union(u, v)
+    else:
+        enemy[u].append(v)
+        enemy[v].append(u)
+
+for i in enemy.keys():
+    for e in enemy[i]:
+        for f in enemy[e]:
+            union(i, f)
+
+cnt = set()
+for p in parent[1:]:
+    p = find(p)
+    if p not in cnt:
+        cnt.add(p)
+
+print(len(cnt))
