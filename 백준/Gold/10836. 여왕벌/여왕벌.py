36d4b48cb06d872ce0dac66d1eeeafc9c70d63a1
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-07-17 16:26:56 +0900

    [Gold III] Title: 여왕벌, Time: 2484 ms, Memory: 116356 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -2,29 +2,27 @@ import sys
 input = sys.stdin.readline
 
 M, N = map(int, input().split())
-board = [[1]*M for _ in range(M)]
 
-def side(arr):
-    idx = 0
-    for r in range(M-1, -1, -1):
-        while arr[idx] == 0:
-            idx += 1
-        board[r][0] += idx
-        arr[idx] -= 1
-    for c in range(1, M):
-        while arr[idx] == 0:
-            idx += 1
-        board[0][c] += idx
-        arr[idx] -= 1
-    
-def grow():
-    for i in range(1, M):
-        for j in range(1, M):
-            board[i][j] = max(board[i-1][j], board[i][j-1], board[i-1][j-1])
+L = 2*M-1
+size = [1]*L
 
 for _ in range(N):
-    arr = list(map(int, input().split()))
-    side(arr)
-grow()
+    a,b,c = map(int, input().split())
+    for i in range(a, a+b):
+        size[i] += 1
+    for i in range(a+b, 2*M-1):
+        size[i] += 2
 
-print('\n'.join(' '.join(map(str,row)) for row in board))
+board = [[0]*M for _ in range(M)]
+
+for i in range(M):
+    board[M - 1 - i][0] = size[i]
+for j in range(1, M):
+    board[0][j] = size[M - 1 + j]
+
+for i in range(1, M):
+    for j in range(1, M):
+        board[i][j] = max(board[i-1][j], board[i][j-1], board[i-1][j-1])
+
+for row in board:
+    print(*row)
