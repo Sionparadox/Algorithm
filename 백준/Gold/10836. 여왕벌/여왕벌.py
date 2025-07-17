17e541310a73cb630e60aceefef37cc9b18d77d4
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-07-17 14:45:10 +0900

    [Gold III] Title: 여왕벌, Time: 140 ms, Memory: 37140 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+board = [[1]*M for _ in range(M)]
+
+def side(arr):
+    idx = 0
+    for r in range(M-1, -1, -1):
+        while arr[idx] == 0:
+            idx += 1
+        board[r][0] += idx
+        arr[idx] -= 1
+    for c in range(1, M):
+        while arr[idx] == 0:
+            idx += 1
+        board[0][c] += idx
+        arr[idx] -= 1
+    
+def grow():
+    for i in range(1, M):
+        for j in range(1, M):
+            board[i][j] = max(board[i-1][j], board[i][j-1])
+
+for _ in range(N):
+    arr = list(map(int, input().split()))
+    side(arr)
+    grow()
+
+print('\n'.join(' '.join(map(str,row)) for row in board))
