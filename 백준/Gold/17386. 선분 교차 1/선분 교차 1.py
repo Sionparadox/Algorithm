4afc934f3f89eb485f85c05f126d71ada9fa73a9
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-08-24 16:48:37 +0900

    [Gold III] Title: 선분 교차 1, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+def CCW(x1, y1, x2, y2, x3, y3):
+    return (x2-x1)*(y3-y1) - (x3-x1)*(y2-y1)
+
+def is_intersect(x1, y1, x2, y2, x3, y3, x4, y4):
+    a = CCW(x1, y1, x2, y2, x3, y3)
+    b = CCW(x1, y1, x2, y2, x4, y4)
+    c = CCW(x3, y3, x4, y4, x1, y1)
+    d = CCW(x3, y3, x4, y4, x2, y2)
+    if a*b <= 0 and c*d <= 0:
+        return True
+    return False
+
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+
+if is_intersect(x1, y1, x2, y2, x3, y3, x4, y4):
+    print(1)
+else:
+    print(0)
