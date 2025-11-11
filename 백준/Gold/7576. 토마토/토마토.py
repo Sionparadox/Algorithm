b7fc1d8919ecc9cb308ec076a3417cf4cbb8da58
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-11-11 16:11:31 +0900

    [Gold V] Title: 토마토, Time: 2152 ms, Memory: 100472 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -4,7 +4,6 @@ input = sys.stdin.readline
 
 M, N = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
 
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 queue = deque()
@@ -19,19 +18,18 @@ while queue:
     answer += 1
     for _ in range(L):
         r, c = queue.popleft()
-        visited[r][c] = True
         for dr ,dc in directions:
             nr, nc = r+dr, c+dc
             if nr<0 or nr>=N or nc<0 or nc>=M:
                 continue
-            if board[nr][nc] == 0 and not visited[nr][nc]:
-                visited[nr][nc] = True
+            if board[nr][nc] == 0:
+                board[nr][nc] = 1
                 queue.append((nr, nc))
 
 
 for r in range(N):
     for c in range(M):
-        if not visited[r][c] and board[r][c] == 0:
+        if board[r][c] == 0:
             answer = -1
             break
 print(answer)
