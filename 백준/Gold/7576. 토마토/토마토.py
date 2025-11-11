6ba7140449489e80a477aa68c0822115d71736ab
시온 <99112680+Sionparadox@users.noreply.github.com>
2025-11-11 16:10:09 +0900

    [Gold V] Title: 토마토, Time: 2540 ms, Memory: 108324 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
+
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+queue = deque()
+for r in range(N):
+    for c in range(M):
+        if board[r][c] == 1:
+            queue.append((r, c))
+
+answer = -1
+while queue:
+    L = len(queue)
+    answer += 1
+    for _ in range(L):
+        r, c = queue.popleft()
+        visited[r][c] = True
+        for dr ,dc in directions:
+            nr, nc = r+dr, c+dc
+            if nr<0 or nr>=N or nc<0 or nc>=M:
+                continue
+            if board[nr][nc] == 0 and not visited[nr][nc]:
+                visited[nr][nc] = True
+                queue.append((nr, nc))
+
+
+for r in range(N):
+    for c in range(M):
+        if not visited[r][c] and board[r][c] == 0:
+            answer = -1
+            break
+print(answer)
+
+
+'''
+leveling BFS?
+
+'''
