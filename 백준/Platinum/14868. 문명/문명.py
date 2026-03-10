be48ec369586d47eb5425b749f97616b9c249a02
시온 <99112680+Sionparadox@users.noreply.github.com>
2026-03-10 19:33:25 +0900

    [Platinum IV] Title: 문명, Time: 1900 ms, Memory: 234016 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/14868. 문명/문명.py
+++ 백준/Platinum/14868. 문명/문명.py
@@ -1,78 +1,71 @@
 import sys
 from collections import deque
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-parent = [x for x in range(K)]
-size = [1]*K
-start = []
-lands = [[-1]*N for _ in range(N)]
-for i in range(K):
+parent = [i for i in range(K + 1)]
+lands = [[0] * N for _ in range(N)]
+queue = deque()
+
+for i in range(1, K + 1):
     r, c = map(int, input().split())
-    start.append((r-1, c-1))
     lands[r-1][c-1] = i
+    queue.append((r-1, c-1))
 
 def find(node):
-    if node == parent[node]:
+    if parent[node] == node:
         return node
     parent[node] = find(parent[node])
     return parent[node]
 
 def union(u, v):
     u, v = find(u), find(v)
-    if u == v:
-        return False
-    if size[u] < size[v]:
-        u, v = v, u
-    parent[v] = u
-    size[u] += size[v]
-    return True
+    if u != v:
+        parent[v] = u
+        return True
+    return False
 
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-queue = deque(start)
 
 cnt = 0
 
-def check(r, c):
-    curr = lands[r][c]
-    res = 0
+temp_q = list(queue)
+for r, c in temp_q:
+    current_civ = lands[r][c]
     for dr, dc in directions:
-        nr, nc = r+dr, c+dc
-        if nr<0 or nr>=N or nc<0 or nc>=N:
-            continue
-        if lands[nr][nc] != -1 and lands[nr][nc] != curr:
-            if union(curr, lands[nr][nc]):
-                res += 1
-    return res
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < N and 0 <= nc < N:
+            neighbor_civ = lands[nr][nc]
+            if neighbor_civ != 0 and neighbor_civ != current_civ:
+                if union(current_civ, neighbor_civ):
+                    cnt += 1
 
-for r, c in start:
-    cnt += check(r, c)
-
-if cnt == K-1:
+if cnt == K - 1:
     print(0)
     exit(0)
 
 answer = 0
 while queue:
-    answer += 1
+    
     for _ in range(len(queue)):
         r, c = queue.popleft()
+        
         for dr, dc in directions:
-            nr, nc = r+dr, c+dc
-            if nr<0 or nr>=N or nc<0 or nc>=N:
-                continue
-            if lands[nr][nc] == -1:
-                lands[nr][nc] = lands[r][c]
-                queue.append((nr, nc))
-                cnt += check(nr, nc)
-                if cnt == K-1:
-                    print(answer)
-                    exit(0)
-                
-
-
-'''
-Leveling BFS로 문명 확장
-확장한 위치 주변에 다른 문명이 있는지 검사
-'''
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < N and 0 <= nc < N:
+                if lands[nr][nc] == 0:
+                    lands[nr][nc] = lands[r][c]
+                    queue.append((nr, nc))
+                    
+                    for dr, dc in directions:
+                        nnr, nnc = nr + dr, nc + dc
+                        if 0 <= nnr < N and 0 <= nnc < N:
+                            if lands[nnr][nnc] != 0 and lands[nnr][nnc] != lands[nr][nc]:
+                                if union(lands[nr][nc], lands[nnr][nnc]):
+                                    cnt += 1
+    
+    answer += 1
+    
+    if cnt == K - 1:
+        print(answer)
+        break
