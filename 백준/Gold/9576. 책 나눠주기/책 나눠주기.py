4c64420ec818c2ecb0de0b71bcff41d76e5fe332
시온 <99112680+Sionparadox@users.noreply.github.com>
2026-03-12 16:36:34 +0900

    [Gold II] Title: 책 나눠주기, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
+++ 백준/Gold/9576. 책 나눠주기/책 나눠주기.py
@@ -1,21 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+def find(node):
+    if parent[node] == node:
+        return node
+    parent[node] = find(parent[node])
+    return parent[node]
 
+T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
     rng = [tuple(map(int, input().split())) for _ in range(M)]
     rng.sort(key=lambda x:x[1])
     
-    visited = [False]*(N+1)
+    parent = list(range(N+2))
     answer = 0
     
     for left, right in rng:
-        for i in range(left, right+1):
-            if not visited[i]:
-                visited[i] = True
-                answer += 1
-                break
+        book = find(left)
+        if book <= right:
+            answer += 1
+            parent[book] = find(book+1)
     
     print(answer)
+
+
+'''
+N<=1000 이므로 N^2으로도 풀림
+union-find사용해 경로압축 가능
+'''
