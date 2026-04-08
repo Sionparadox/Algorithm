85e6486a9cc33c955879b15540e6dfd477aa2885
시온 <99112680+Sionparadox@users.noreply.github.com>
2026-04-08 15:37:52 +0900

    [Gold IV] Title: 미로만들기, Time: 64 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,27 +1,32 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-INF = float('inf')
 
 N = int(input())
 board = [list(map(int, list(input().strip()))) for _ in range(N)]
 
-visited = [[INF]*N for _ in range(N)]
+visited = [[-1]*N for _ in range(N)]
 visited[0][0] = 0
 queue = deque([(0, 0)])
 
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 while queue:
     r, c = queue.popleft()
+    if r == N-1 and c == N-1:
+        print(visited[r][c])
+        break
     
     for dr, dc in directions:
         nr, nc = r+dr, c+dc
         if nr<0 or nr>=N or nc<0 or nc>=N:
             continue
-        ncnt = visited[r][c] + 1 - board[nr][nc]
-        if visited[nr][nc] <= ncnt:
+        if visited[nr][nc] != -1:
             continue
-        visited[nr][nc] = ncnt
-        queue.append((nr, nc))
+        
+        if board[nr][nc] == 1:
+            visited[nr][nc] = visited[r][c]
+            queue.appendleft((nr, nc))
+        else:
+            visited[nr][nc] = visited[r][c] + 1
+            queue.append((nr, nc))
 
-print(visited[N-1][N-1])
