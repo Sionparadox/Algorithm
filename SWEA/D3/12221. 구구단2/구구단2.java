34738cda6b5370d39a68094010b0d599f725262d
시온 <99112680+Sionparadox@users.noreply.github.com>
2026-07-12 18:45:08 +0900

    [D3] Title: 구구단2, Time: 74 ms, Memory: 25,216 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= TC; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int answer = A<10 && B<10 ? A*B : -1;
+            System.out.printf("#%d %d\n", tc, answer);
+
+        }
+
+    }
+}
